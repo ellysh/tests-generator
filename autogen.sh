8d832c5425b45e844a9f3e435b03d3d91d28790b
Ilya Shpigor <i.shpigor@samsung.com>
2013-08-21 12:36:36 +0400

    Refactoring of the `autogen.sh` script

--- autogen.sh
+++ autogen.sh
@@ -10,25 +10,56 @@ OBJECT_FILES="$TEMPLATES_DIR/object/*"
 TEMPLATE_FILE="template"
 PARAM_FILE="params.py"
 
+BUG=""
+MEMORY=""
+ACCESS=""
+OBJECT=""
 
-for BUG in $BUG_FILES
+copy_template()
+{
+    cp "$BUG" "$TEMPLATE_FILE"
+}
+
+create_param_file()
+{
+    # Remove params.pyc file
+    rm $PARAM_FILE"c"
+
+    cat "$MEMORY" > "$PARAM_FILE"
+    cat "$ACCESS" >> "$PARAM_FILE"
+    cat "$OBJECT" >> "$PARAM_FILE"
+}
+
+get_out_filename()
+{
+    echo "$(basename "$BUG")"_"$(basename "$ACCESS")"_"$(basename "$MEMORY")"_"$(basename "$OBJECT").c"
+}
+
+generate_test()
+{
+    copy_template
+
+    create_param_file
+
+    OUT_FILE=$(get_out_filename)
+    ./generator.py "$TEMPLATE_FILE" "$TESTS_DIR/$OUT_FILE"
+}
+
+
+for BUG_I in $BUG_FILES
 do
-    for ACCESS in $ACCESS_FILES
+    for ACCESS_I in $ACCESS_FILES
     do
-        for MEMORY in $MEMORY_FILES
+        for MEMORY_I in $MEMORY_FILES
         do
-           for OBJECT in $OBJECT_FILES
+           for OBJECT_I in $OBJECT_FILES
             do
-                OUT_FILE="$(basename "$BUG")"_"$(basename "$ACCESS")"_"$(basename "$MEMORY")"_"$(basename "$OBJECT").c"
+                BUG="$BUG_I"
+                MEMORY="$MEMORY_I"
+                ACCESS="$ACCESS_I"
+                OBJECT="$OBJECT_I"
 
-                cat "$BUG" > "$TEMPLATE_FILE"
-
-                cat "$MEMORY" > "$PARAM_FILE"
-                cat "$ACCESS" >> "$PARAM_FILE"
-                cat "$OBJECT" >> "$PARAM_FILE"
-
-                rm $PARAM_FILE"c"
-                ./generator.py "$TEMPLATE_FILE" "$TESTS_DIR/$OUT_FILE"
+                generate_test
             done
         done
     done
